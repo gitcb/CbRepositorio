8acbb2e5fe9c96de3ba5cb920ef3ffb5fa270662
270246 <270246@35168862-LAB203.utramig.local>
2014-08-14 19:28:31 -0300

    Versao2

--- ProjetoGit/src/GitProjet/GitTeste.java
+++ ProjetoGit/src/GitProjet/GitTeste.java
@@ -1,5 +1,7 @@
 package GitProjet;
 
+import java.util.Scanner;
+
 public class GitTeste {
 
 	/**
@@ -7,7 +9,26 @@ public class GitTeste {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		/*
+		 * 2. Faça um algoritmo que receba dois números e ao final mostre a
+		 * soma, subtração, multiplicação e a divisão dos números lidos.
+		 */
+
+		Scanner valor = new Scanner(System.in);
+		System.out.println("Informe o primeiro numero");
+		int x = valor.nextInt();
+		System.out.println("Informe o segundo numero");
+		int y = valor.nextInt();
+		int soma = x + y;
+		int subtracao = x - y;
+		
+		System.out.println("Soma dos numero é = " + soma);
+		System.out.println("Subtração dos numeros é = " + subtracao);
+		
 
 	}
 
 }
+
+
